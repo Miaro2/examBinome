85a5b807b157411390406939e0fa68bb27f50472
Miaro2 <miarorkt290906@gmail.com>
2025-07-14 10:26:00 +0300

    Second git

--- inc/function.php
+++ inc/function.php
@@ -5,7 +5,7 @@ function connection($mail, $mdp){
     $requete = "SELECT * FROM gestion_emprunt_membre WHERE email = '%s' AND mdp = '%s'";
     $requete = sprintf($requete, $mail, $mdp);
 
-    $resultat = mysqli_query($dataBase, $requete);
+    $resultat = mysqli_query(getdataBase(), $requete);
     $donne = mysqli_fetch_assoc($resultat);
 
     if ($donne) {
@@ -16,7 +16,29 @@ function connection($mail, $mdp){
         header("Location: ../pages/accueil.php");
     } else {
         $_SESSION['error'] = "Erreur de connexion";
-        header("Location: ../pages/index.php");
+        header("Location: ../pages/login.php");
     }
+}
+
+function create_compte($nom, $mail, $mdp, $date, $genre, $ville) {
+    $checkEmailQuery = "SELECT * FROM gestion_emprunt_membre WHERE email = '%s'";
+    $checkEmailQuery = sprintf($checkEmailQuery, $mail);
+    $checkEmailResult = mysqli_query(getdataBase(), $checkEmailQuery);
+
+    if (mysqli_num_rows($checkEmailResult) > 0) {
+        $_SESSION['deja'] = "Email déjà utilisé";
+        header("Location: ../pages/inscription.php");
+        exit;
+    }
+
+    $insert = "INSERT INTO gestion_emprunt_membre (nom, email, mdp, date_naissance, genre ,ville) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')";
+    $insert = sprintf($insert, $nom, $mail, $mdp, $date, $genre, $ville);
+
+    if (mysqli_query(getdataBase(), $insert)) {
+        header("Location: ../pages/login.php");
+        exit;
+    }
+
+    header("Location: ../pages/inscription.php");
 }
 ?>
---
+++ pages/inscription.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+// require('../inc/functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="..assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
+            <a href="index.php"><h1 style="color: rgb(63, 66, 66);">Login</h1>
+            <a href="#"><h1 style="color: rgb(0, 112, 97);">Sign up</h1></a>
+        <form action="traitement_signup.php" method="get">
+            <div class="test">
+                <p>Nom</p>
+                <input type="text" name="name" placeholder="Nom" id="input">
+                <p>EMail</p>
+                <input type="email" name="mail" placeholder = "EMail" id="input">
+                <p>Date de naissance</p>
+                <input type="date" name="date" id="input">
+                <p>Mot de passe</p>
+                <input type="password" name="mdp" placeholder="Mot de passe" id="input">
+                <p>Genre</p>
+                <select name="genre" id="">
+                    <option value="H">Homme</option>
+                    <option value="F">Femme</option>
+                </select>
+                <p>Ville</p>
+                <input type="text" name="ville" placeholder="Ville" id="input">
+                <br>
+                <input type="submit" value="Validez" id="submit">
+        </form>
+
+</body>
+</html>
